01e370d4712ed0998f74048cbe5b5a5e3a4f237a
yafira <yafira.martinez@lc.cuny.edu>
2023-10-31 18:38:58 -0400

    changed txtarea width to 300px

--- src/components/WeekCalendar.js
+++ src/components/WeekCalendar.js
@@ -59,7 +59,7 @@ const WeekCalendar = ({ startDate }) => {
 					value={dayTexts[i]}
 					onChange={(e) => handleDayTextChange(i, e.target.value)}
 					style={{
-						width: '280px',
+						width: '300px',
 						padding: '5px',
 						margin: '0',
 						minHeight: '250px',
